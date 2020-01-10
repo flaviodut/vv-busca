1b3516dee3fa1334a13776c5ad5be96d36f53f5a
Ricardo de C Rodrigues Ribeiro <ricardo.ribeiro-ext@viavarejo.com.br>
2020-01-10 16:50:17 -0300

    ajuste sistema de grid

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 
+import Row from '../../commons/layout/row'
+import Grid from '../../commons/layout/grid'
+
 export default props => (
-    <Link to="/">home</Link>
+    <Row>
+        <Grid cols='6'>
+            <Link to="/">home</Link>
+        </Grid>
+        <Grid cols='6'>
+            <Link to="/">home</Link>
+        </Grid>
+    </Row>
 )

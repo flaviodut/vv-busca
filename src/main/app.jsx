7a1164c864177a57324a9b6be07c98fb73d5b416
Ricardo de C Rodrigues Ribeiro <ricardo.ribeiro-ext@viavarejo.com.br>
2020-01-10 15:58:29 -0300

    criação de componentes genericos

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -4,7 +4,7 @@ import Router from './routes'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 export default props => (
-    <div>
+    <div className="container">
         <Router/>
     </div>
 )

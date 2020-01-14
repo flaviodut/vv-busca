7732b7cdbf09e6a48cdfebc27fae35cd4b7b2d80
Flávio Dutra <flavio.adore@gmail.com>
2020-01-14 14:48:44 -0300

    Adicionado opção com radio buttons para mudança de Situação

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -7,11 +7,16 @@ import Grid from '../../commons/layout/grid';
 
 
 export default props => {
+    const Table = styled.table`
+        thead th:last-of-type {
+            width: 50%;
+        }`
+
     const InputNumeric = styled.div`
-        input {
-            max-width: 3rem;
-        }
-    `
+        input.form-control {
+            max-width: 3.5rem;
+            min-width: 2.5rem;
+        }`
 
     const Pagination = styled.ul`
         .page-link {
@@ -21,6 +26,12 @@ export default props => {
         .page-item.active .page-link {
             background-color: #6c757d;
             border-color: #6c757d;
+        }`
+
+    const ToggleStatus = styled.div`
+        .btn-success.active {
+            color: #fff;
+            background-color: #28a745;
         }
     `
 
@@ -69,13 +80,13 @@ export default props => {
             <Row>
                 <Grid cols="12">
                     <div className="table-responsive mb-5">
-                        <table className="table table-bordered">
+                        <Table className="table table-bordered">
                             <thead className="thead-light">
                                 <tr>
                                     <th scope="col">Situação</th>
                                     <th scope="col">Prioridade</th>
                                     <th scope="col">Nível</th>
-                                    <th scope="col" className="w-50">Nome</th>
+                                    <th scope="col">Nome</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -85,7 +96,7 @@ export default props => {
                                     </td>
                                     <td>0,96</td>
                                     <td>
-                                        <InputNumeric className="input-group input-group-sm">
+                                        <InputNumeric className="input-group input-group-sm flex-nowrap">
                                             <div className="input-group-prepend">
                                                 <button className="btn btn-secondary px-1" type="button" id="buttonSub"><span className="fa fa-angle-down"></span></button>
                                             </div>
@@ -103,7 +114,7 @@ export default props => {
                                     </td>
                                     <td>0,95</td>
                                     <td>
-                                        <InputNumeric className="input-group input-group-sm">
+                                        <InputNumeric className="input-group input-group-sm flex-nowrap">
                                             <div className="input-group-prepend">
                                                 <button className="btn btn-secondary px-1" type="button" id="buttonSub"><span className="fa fa-angle-down"></span></button>
                                             </div>
@@ -121,7 +132,7 @@ export default props => {
                                     </td>
                                     <td>0,2</td>
                                     <td>
-                                        <InputNumeric className="input-group input-group-sm">
+                                        <InputNumeric className="input-group input-group-sm flex-nowrap">
                                             <div className="input-group-prepend">
                                                 <button className="btn btn-secondary px-1" type="button" id="buttonSub"><span className="fa fa-angle-down"></span></button>
                                             </div>
@@ -134,7 +145,110 @@ export default props => {
                                     <td>Xiaomi Redmi</td>
                                 </tr>
                             </tbody>
-                        </table>
+                        </Table>
+                    </div>
+                </Grid>
+            </Row>
+
+            <Row>
+                <Grid cols="12">
+                    <div className="table-responsive mb-5">
+                        <Table className="table table-bordered">
+                            <thead className="thead-light">
+                                <tr>
+                                    <th scope="col">Situação</th>
+                                    <th scope="col">Prioridade</th>
+                                    <th scope="col">Nível</th>
+                                    <th scope="col">Nome</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        <ToggleStatus className="btn-group btn-group-toggle btn-group-sm" data-toggle="buttons">
+                                            <label className="btn btn-success">
+                                                <input type="radio" name="options" id="option1" autoComplete="off" defaultChecked /><span className="fa fa-check"></span>
+                                            </label>
+                                            <label className="btn btn-light">
+                                                <input type="radio" name="options" id="option2" autoComplete="off" /><span className="fa fa-minus"></span>
+                                            </label>
+                                            <label className="btn btn-light">
+                                                <input type="radio" name="options" id="option3" autoComplete="off" /><span className="fa fa-times"></span>
+                                            </label>
+                                        </ToggleStatus>
+                                    </td>
+                                    <td>0,96</td>
+                                    <td>
+                                        <InputNumeric className="input-group input-group-sm flex-nowrap">
+                                            <div className="input-group-prepend">
+                                                <button className="btn btn-secondary px-1" type="button" id="buttonSub"><span className="fa fa-angle-down"></span></button>
+                                            </div>
+                                            <input type="text" className="form-control input-number text-center" defaultValue="0" min="0" max="100" maxLength="3" />
+                                            <div className="input-group-append">
+                                                <button className="btn btn-secondary px-1" type="button" id="buttonAdd"><span className="fa fa-angle-up"></span></button>
+                                            </div>
+                                        </InputNumeric>
+                                    </td>
+                                    <td>Samsung Galaxy S9</td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <ToggleStatus className="btn-group btn-group-toggle btn-group-sm" data-toggle="buttons">
+                                            <label className="btn btn-light">
+                                                <input type="radio" name="options" id="option1" autoComplete="off" /><span className="fa fa-check"></span>
+                                            </label>
+                                            <label className="btn btn-warning">
+                                                <input type="radio" name="options" id="option2" autoComplete="off" defaultChecked /><span className="fa fa-minus"></span>
+                                            </label>
+                                            <label className="btn btn-light">
+                                                <input type="radio" name="options" id="option3" autoComplete="off" /><span className="fa fa-times"></span>
+                                            </label>
+                                        </ToggleStatus>
+                                    </td>
+                                    <td>0,95</td>
+                                    <td>
+                                        <InputNumeric className="input-group input-group-sm flex-nowrap">
+                                            <div className="input-group-prepend">
+                                                <button className="btn btn-secondary px-1" type="button" id="buttonSub"><span className="fa fa-angle-down"></span></button>
+                                            </div>
+                                            <input type="text" className="form-control input-number text-center" defaultValue="0" min="0" max="10" maxLength="2" />
+                                            <div className="input-group-append">
+                                                <button className="btn btn-secondary px-1" type="button" id="buttonAdd"><span className="fa fa-angle-up"></span></button>
+                                            </div>
+                                        </InputNumeric>
+                                    </td>
+                                    <td>Samsung Galaxy S8</td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <ToggleStatus className="btn-group btn-group-toggle btn-group-sm" data-toggle="buttons">
+                                            <label className="btn btn-light">
+                                                <input type="radio" name="options" id="option1" autoComplete="off" /><span className="fa fa-check"></span>
+                                            </label>
+                                            <label className="btn btn-light">
+                                                <input type="radio" name="options" id="option2" autoComplete="off" /><span className="fa fa-minus"></span>
+                                            </label>
+                                            <label className="btn btn-danger">
+                                                <input type="radio" name="options" id="option3" autoComplete="off" defaultChecked /><span className="fa fa-times"></span>
+                                            </label>
+                                        </ToggleStatus>
+                                    </td>
+                                    <td>0,2</td>
+                                    <td>
+                                        <InputNumeric className="input-group input-group-sm flex-nowrap">
+                                            <div className="input-group-prepend">
+                                                <button className="btn btn-secondary px-1" type="button" id="buttonSub"><span className="fa fa-angle-down"></span></button>
+                                            </div>
+                                            <input type="text" className="form-control input-number text-center" defaultValue="0" min="0" max="10" maxLength="2" />
+                                            <div className="input-group-append">
+                                                <button className="btn btn-secondary px-1" type="button" id="buttonAdd"><span className="fa fa-angle-up"></span></button>
+                                            </div>
+                                        </InputNumeric>
+                                    </td>
+                                    <td>Xiaomi Redmi</td>
+                                </tr>
+                            </tbody>
+                        </Table>
                     </div>
                 </Grid>
             </Row>

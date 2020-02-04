a8457dd12219f48112b8a899b3480647b7a7baca
flaviodut <flavio.adore@gmail.com>
2020-02-04 18:55:06 -0300

    Adicionei o botão para carregar mais produtos

--- src/commons/template/search-grid.jsx
+++ src/commons/template/search-grid.jsx
@@ -1,38 +1,60 @@
 import React from 'react'
 import styled from 'styled-components'
 
-import SearchProductCard from './product-card'
-import SearchProductControl from './search-controls'
+import Row from '../layout/row'
+import Grid from '../layout/grid'
+import SearchControls from './search-controls'
+import ProductCard from './product-card'
 
 export default class SearchGrid extends React.Component {
   render() {
-    const Grid = styled.ul`
+    const GridProducts = styled.ul`
       display: flex;
       flex-direction: row;
       flex-wrap: wrap;
-      margin-left: -15px;
-      margin-right: -15px;
+      margin-left: -1rem;
+      margin-right: -1rem;
 
       li {
-        padding: 0 15px;
+        padding: 0 1rem;
         width: 50%;
 
         @media (min-width: 992px) {
           width: 33.33333%;
         }
       }
-    `
+
+      `
+
+      const LoadMore = styled.button`
+        background-color: var(--buttonSecondaryColor);
+        border-color: var(--buttonSecondaryColor);
+        color: #fff;
+        font-weight: 700;
+
+        :hover,
+        :focus {
+          background-color: var(--buttonSecondaryColorHover);
+          border-color: var(--buttonSecondaryColorHover);
+          color: #fff;
+        }
+      `
 
     return (
       <div>
-        <SearchProductControl />
-        <Grid className="list-unstyled">
-          <SearchProductCard />
-          <SearchProductCard />
-          <SearchProductCard />
-          <SearchProductCard />
-          <SearchProductCard />
-        </Grid>
+        <SearchControls />
+        <GridProducts className="list-unstyled">
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+        </GridProducts>
+        <Row addClass="justify-content-center">
+          <Grid cols="12 12 8 4">
+            <LoadMore className="btn btn-block">Ver mais produtos</LoadMore>
+          </Grid>
+        </Row>
       </div>
     )
   }

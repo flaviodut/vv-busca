183fb5b76b5424e927a1028c10be08744f6bb0a9
flaviodut <flavio.adore@gmail.com>
2020-02-04 18:55:26 -0300

    Adicionado controle

--- src/commons/template/search-controls.jsx
+++ src/commons/template/search-controls.jsx
@@ -1,9 +1,54 @@
 import React from 'react'
+import styled from 'styled-components'
 
 export default class SearchProductControl extends React.Component {
   render() {
+    const Controls = styled.div`
+      align-items: center;
+      display: flex;
+      font-size: .875rem;
+      justify-content: space-between;
+      margin-bottom: 1.875em;
+      padding-left: 1rem;
+      padding-right: 1rem;
+
+      p {
+        margin-bottom: 0;
+      }
+    `
+    
+    const ControlField = styled.div`
+      display: flex;
+      align-items: center;
+
+      label {
+        margin-bottom: 0;
+        margin-right: 1rem;
+        white-space: nowrap;
+      }
+
+      select {
+        border-color: #c0c0c0;
+        color: var(--textColor);
+        font-size: .875rem;
+        font-weight: 700;
+      }
+    `
+
     return (
-      <p>controle</p>
+      <Controls>
+        <p><b>65</b> produtos encontrados</p>
+        <ControlField>
+          <label>Ordenar por</label>
+          <select className="form-control">
+            <option sort="1">Relevância</option>
+            <option sort="16">Maior desconto</option>
+            <option sort="5">Menor preço</option>
+            <option sort="6">Maior preço</option>
+            <option sort="11">Novidades</option>
+          </select>
+        </ControlField>
+      </Controls>
     )
   }
 }
